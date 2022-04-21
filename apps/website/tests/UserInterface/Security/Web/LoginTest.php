b1ae8528c5da2a396184350e98408d5cf5aea628
Alexandre Gérault <alexandre@hexium.io>
2022-04-21 21:54:56 +0200

    test(security): authenticate route

--- apps/website/tests/UserInterface/Security/Web/LoginTest.php
+++ apps/website/tests/UserInterface/Security/Web/LoginTest.php
@@ -1,7 +1,13 @@
 <?php
 
 use function Pest\Laravel\get;
+use function Pest\Laravel\post;
 
 it('shows the login page', function () {
     get(route('security.login'))->assertSuccessful()->assertSee("Me connecter");
+});
+
+it('authenticates a user', function () {
+    post(route('security.authenticate'), ['email' => 'user@email', 'password' => 'password'])->assertSuccessful();
+    expect(auth()->check())->toBeTrue();
 });
