b7c3cf85bf2e1a7121dd6ebe4bf127507d60de10
Alexandre Gérault <alexandre@hexium.io>
2022-04-22 12:49:31 +0200

    feat(login): add form to view

--- apps/website/resources/views/security/login.blade.php
+++ apps/website/resources/views/security/login.blade.php
@@ -1,1 +1,18 @@
-Me connecter
+<form action="{{ route('security.authenticate') }}" method="post">
+    @csrf
+    <label>
+        Adresse mail
+        <input type="email" name="email"/>
+    </label>
+    @foreach($errors->get('email') as $error)
+        <p>{{$error}}</p>
+    @endforeach
+
+    <label>
+        Mot de passe
+        <input type="password" name="password"/>
+    </label>
+
+    @dump($errors)
+    <button type="submit">Me connecter</button>
+</form>
